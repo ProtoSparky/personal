a38477deac75f32e8ad271860882bda6b098698f
Sparky <contact@protosparky.uk>
2024-02-15 16:03:39 +0100

    modified

--- search4ai/curl2.py
+++
@@ -1,6 +0,0 @@
-import requests 
-URL = "https://wikipedia.com" 
-headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"} 
-# Here the user agent is for Edge browser on windows 10. You can find your browser user agent from the above given link. 
-r = requests.get(url=URL, headers=headers) 
-print(r.content)
