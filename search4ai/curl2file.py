5a0ad1e9565df96e8461c3e2117ca6a4b3a71fce
Sparky <contact@protosparky.uk>
2024-02-15 16:15:41 +0100

    it's finally working YESSSS

--- search4ai/curl2file.py
+++ search4ai/curl2file.py
@@ -1,5 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
+############################################################################################
+############################################################################################
+############################################################################################
+#this script copies all data from a website
 
 def scrape_website(url):
     # Send a GET request to the URL
@@ -17,6 +23,85 @@ def scrape_website(url):
         print("The content has been saved to output.html")
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+############################################################################################
+############################################################################################
+############################################################################################
+
+#this script copies everything, and tries to filter away css and js
+
+def scrape_and_filter_website(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful (status code  200)
+    if response.status_code ==  200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Remove script tags, noscript tags, style tags, and svg tags
+        for tag in soup(['script', 'noscript', 'style', 'svg']):
+            tag.decompose()
+        
+        # Remove inline styles
+        for tag in soup.find_all(True):
+            if 'style' in tag.attrs:
+                del tag['style']
+        
+        # Remove class and id attributes
+        for tag in soup.find_all(True):
+            if 'class' in tag.attrs:
+                del tag['class']
+            if 'id' in tag.attrs:
+                del tag['id']
+        
+        # Save the parsed HTML to a file
+        with open('./output.html', 'w', encoding='utf-8') as f:
+            f.write(str(soup))
+        
+        print("The content has been saved to output.html")
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+############################################################################################
+############################################################################################
+############################################################################################
+
+#this function scrapes and converts the text in a website to a txt file
+def read_website(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful (status code   200)
+    if response.status_code ==   200:
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Remove script tags, noscript tags, style tags, and svg tags
+        for tag in soup(['script', 'noscript', 'style', 'svg']):
+            tag.decompose()
+        
+        # Remove inline styles
+        for tag in soup.find_all(True):
+            if 'style' in tag.attrs:
+                del tag['style']
+        
+        # Remove class and id attributes
+        for tag in soup.find_all(True):
+            if 'class' in tag.attrs:
+                del tag['class']
+            if 'id' in tag.attrs:
+                del tag['id']
+        
+        # Extract text nodes containing more than one word
+        text_nodes = [node for node in soup.stripped_strings if len(node.split()) >  1]
+        
+        # Save the extracted text to a file
+        with open('./extracted_text.txt', 'w', encoding='utf-8') as f:
+            for text in text_nodes:
+                f.write(text + '\n')
+        
+        print("The content has been saved to extracted_text.txt")
+    else:
+        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
 # Example usage:
-scrape_website("https://www.cnet.com/home/internet/features/the-secret-life-of-the-500-cables-that-run-the-internet/")
+read_website("https://www.cnet.com/home/internet/features/the-secret-life-of-the-500-cables-that-run-the-internet/")
