2ab3fde2a96809f63464b7a002af4ab6de30e943
Sparky <contact@protosparky.uk>
2024-02-15 14:05:55 +0100

    working on pysearch

---
+++ search4ai/curl3.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup 
+import requests 
+URL = "https://kuben.vgs.no" 
+r = requests.get(URL) 
+  
+soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib 
+print(soup.prettify()) 
