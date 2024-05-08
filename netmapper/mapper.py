1609103091a8c6154b7b35f4df94cdf700900623
~nz <81528500+ProtoSparky@users.noreply.github.com>
2024-05-08 21:16:18 +0200

    working on personal stuff again

---
+++ netmapper/mapper.py
@@ -0,0 +1,38 @@
+import subprocess
+import platform
+import re
+
+from urllib.parse import urlparse
+
+
+def ping_server(host):
+    """
+    Pings a server and returns the ping time in milliseconds or None if the server cannot be found.
+    """
+    # Parse the host URL to extract the domain or IP address
+    parsed_host = urlparse(host)
+    # If a scheme is present, use the netloc attribute to get the hostname or IP address
+    # If no scheme is present, use the host as is
+    hostname = parsed_host.netloc if bool(parsed_host.scheme) else host
+
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    command = ['ping', param, '1', hostname]
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
+        # Extracting the time value from the output
+        if 'time=' in output:
+            time_index = output.find('time=') +   5
+            time_value = output[time_index:].split()[0]
+            # Returning the time in milliseconds
+            return extract_number_from_ping(time_value)
+    except subprocess.CalledProcessError:
+        pass  # Server not found, return None
+    return None
+
+def extract_number_from_ping(ping_string):
+    # Use regex to find all digits in the string
+    numbers = re.findall(r'\d+', ping_string)
+    # Since you're interested in the first number, convert it to an integer
+    return int(numbers[0]) if numbers else None
+
+print(ping_server("google.com"))
