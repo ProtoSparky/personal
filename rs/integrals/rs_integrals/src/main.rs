1c9bb0c49f2134327c13a5ee7f9da53a60a5260c
~nz <81528500+ProtoSparky@users.noreply.github.com>
2024-05-21 22:23:59 +0200

    cwab

--- rs/integrals/rs_integrals/src/main.rs
+++ rs/integrals/rs_integrals/src/main.rs
@@ -1,6 +1,6 @@
-fn main() {
+fn main() {/*
     let mut start_num = 0;
-    /*
+    
     while true {
         start_num +=1;/*
         let string = start_num.to_string();
@@ -8,9 +8,31 @@ fn main() {
 
 
     }*/
-    let mut prime_flag = false; 
-    let mut prime_pointer = Null;
-    for prime_pointer
+    println!("{}", prime_checker(17))
 
 
+
+
+}
+fn prime_checker(input:u32) -> bool{ //u32 means the number is a 32bit unsigned intiger. "->bool means the return is expected to be a boolean"
+    let mut prime_flag = false;
+    if input <= 1{
+        prime_flag = false; //number is not prime
+    }
+    else if input < 1{
+        for prime_pointer in 2..input { //x..y works the same way as a python range(x,y), generates a list of numbers from x to y
+            if (input/prime_pointer) == 0 {
+                //number factor found, set prime_flag to true
+                prime_flag = true; 
+                break; 
+            }
+        }
+    }
+
+    if prime_flag == true{
+        return true;
+    }
+    else{
+        return false; 
+    }
 }
