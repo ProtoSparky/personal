c203955296b2708ee7592e9c6285964b6ed16cf6
Sparky <81528500+ProtoSparky@users.noreply.github.com>
2024-05-27 13:20:03 +0200

    filter filtered

--- ssid_rate/rater.py
+++ ssid_rate/rater.py
@@ -6,12 +6,13 @@ username = getpass.getuser()
 llamaAPI = "192.168.50.220:11434/api"
 llama_model = "llama3"
 file_path = f"C:\\Users\\{username}\\Downloads\\ssid2.csv"
+stock_ssid = ["AirTies", "Air", "TP-Link", "Telia", "Telenor", 
+"GNX", "Getbox", "NextGenTel", "DIRECT","Home_Net","Zyxel", "NETGEAR", "Altibox", 
+"ASUS", "dlink", "TP-LINK", "Osloskolen", "GET", "Get", "linksys", "REMOTE", "HP-"]
 
 
 def rater(ssid):
     ## Ask AI to rate ssid
-    stock_ssid = ["AirTies%&", "Air%&", "TP-Link%&", "Telia%&", "Telenor%&", 
-                  "GNX%&", "Getbox%&", "NextGenTel%&", "DIRECT%&","Home_Net%&","Zyxel%&", "NETGEAR%&"]
     system_msg = """Your job is to rate the wifi SSID the user sends from 0 to 10 by creativity. 
     If the SSID is a default one, rate it negative 1. 
     You myst ONLY reply with the intiger, and nothing more.
@@ -22,13 +23,37 @@ def rater(ssid):
     total_msg.append(system_msg_obj)
     total_msg.append(user_msg_obj)
     response = Client(llamaAPI).chat(model=llama_model, messages=total_msg)
-    return response["message"]["content"]
+    try: 
+        return response["message"]["content"]
+    except:
+        print(response)
     
-
+def matcher(input_str, arr2match):
+    for element in arr2match:
+        if element in input_str:
+            return True
+    return False
 
 def filter():
+    new_arr = []
     for current_line in tk.read_file_by_lines(file_path):
         if(current_line != ""):
-            print(current_line)
+            if(matcher(current_line, stock_ssid)):
+                pass
+            else:
+                new_arr.append(current_line)
 
-filter()
+    return new_arr
+
+
+
+
+tmp_array = []
+for current_network in filter():
+    rated = rater(current_network)
+    network = {current_network : rated}
+    print(network)
+    tmp_array.append(network)
+
+print(tmp_array)
+    
